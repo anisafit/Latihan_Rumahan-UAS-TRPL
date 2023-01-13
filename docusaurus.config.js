463e916ed18e975776b2240035b10eb55ed546aa
anisafit <118428759+anisafit@users.noreply.github.com>
2023-01-13 20:27:46 +0700

    ganti latihan rumahan ()

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Latihan Rumahan',
+  title: 'Latihan Rumahan ()',
   tagline: 'From groceries to goals, we have got you covered.',
   url: 'https://latihan-rumahan-uas-trpl.vercel.app/',
   baseUrl: '/',
